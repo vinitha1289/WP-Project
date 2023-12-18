65b5338f456db7bc0a6201ae6598ed49a0cebbae
vinitha1289 <113001986+vinitha1289@users.noreply.github.com>
2023-12-18 20:51:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+<script>
+  document.getElementById("bookNowBtn").addEventListener("click", function() {
+    window.location.href = "https://www.google.com/search?client=firefox-b-d&q=travel+website+link"; // Replace this URL with the actual external link
+  });
+</script>
